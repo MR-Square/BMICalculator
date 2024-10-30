0a1cf5ddfbb03f41271d12d99a4a040515c4e3fe
Mohd Raza Shaikh <mohdraza7058@gmail.com>
2024-10-30 14:25:41 +0530

    app colors added

---
+++ lib/utils/app_colors.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primary = Color(0xff1C2135);
+  static const Color appBarColor = Color(0xff24263B);
+  static const Color card1 = Color(0xff333244);
+  static const Color card2 = Color(0xff24263B);
+  static const Color largeBtn = Color(0xffE83D67);
+  static const Color smallBtn = Color(0xff8B8C9E);
+  static const Color sliderColor = Color(0xffE83D67);
+
+  static const Color greyTxtColor = Color(0xff8B8C9E);
+  static const Color whiteTxtColor = Color(0xffFFFFFF);
+}
